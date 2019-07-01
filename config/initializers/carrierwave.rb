69a45e5b740459c84ec06d3fb4795dea16885aeb
西村勇人 <nishimurahayato@nishimurahayatonoMacBook-Air.local>
2019-07-01 17:50:26 +0900

    w

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,6 +11,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1' #例 'ap-northeast-1'
   }
-
-  config.fog_directory  = 'zars'
+  config.fog_directory  = 'zard'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/zard'
+end
